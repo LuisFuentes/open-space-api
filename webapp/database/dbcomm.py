7ec0e1c3ecbe915e453c0780fad1ddcc3f4596f7
Luis <luis90fuentes@gmail.com>
2019-03-06 21:06:57 -0800

    Added Heroku Production DB URL and dev DB URL

--- webapp/database/dbcomm.py
+++ webapp/database/dbcomm.py
@@ -5,16 +5,24 @@ from sqlalchemy.ext.declarative import declarative_base
 from webapp import Parser
 import os
 
-# Fetch the config settings for the DB
-# database_user = str(Parser.get("DatabaseSettings", "User"))
-# database_pw = str(Parser.get("DatabaseSettings", "Password"))
-# database_name = str(Parser.get("DatabaseSettings", "DatabaseName"))
-# database_server = str(Parser.get("DatabaseSettings", "ServerName"))
 
-# Send the connection string
-# FORMAT: //user:password@host/dbname[?key=value..]
-# connection_str = "postgresql://{0}:{1}@{2}/{3}".format(database_user, database_pw, database_server, database_name) 
-connection_str = os.environ['DATABASE_URL']
+
+if 'DATABASE_URL' in os.environ:
+    # Heroku Production DB URL
+    connection_str = os.environ['DATABASE_URL']
+else:
+    # Otherwise, use the dev DB URL
+    # Fetch the config settings for the DB
+    database_user = str(Parser.get("DatabaseSettings", "User"))
+    database_pw = str(Parser.get("DatabaseSettings", "Password"))
+    database_name = str(Parser.get("DatabaseSettings", "DatabaseName"))
+    database_server = str(Parser.get("DatabaseSettings", "ServerName"))
+
+    # Send the connection string
+    # FORMAT: //user:password@host/dbname[?key=value..]
+    connection_str = "postgresql://{0}:{1}@{2}/{3}".format(database_user, database_pw, database_server, database_name) 
+
+
 
 engine = create_engine(connection_str, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
